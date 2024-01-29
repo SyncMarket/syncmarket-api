0e45b4d14859cc74de4d09bafa0b37fbdf7df8ad
João Victor Reis <joaovictor.reis2000@gmail.com>
2024-01-29 09:58:42 -0300

    refactor: database script to mongodb script

--- scripts/test-mongodb.ts
+++ scripts/test-mongodb.ts
@@ -2,6 +2,6 @@ import { jestCommand } from './jestCommand';
 
 const args = process.argv.slice(2);
 
-const testDirectory = '__test__/infra/database';
+const testDirectory = '__test__/infra/database/mongodb/repositories';
 
 jestCommand(args, testDirectory);
